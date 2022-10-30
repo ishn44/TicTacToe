9f5649c17f72d405b0ac8b2877defb230a4d962c
Ishan Lalwani <ishn44@gmail.com>
2022-10-30 11:50:12 -0700

    na

--- src/Main/Display.js
+++ src/Main/Display.js
@@ -7,19 +7,28 @@ export const Display = ({ game, handleButtonClick }) => {
     <Container
       sx={{
         maxWidth: { xs: "380px", sm: "900px" },
+        padding: { xs: "5px", sm: "10px" },
       }}
     >
-      <Grid container>
-        {gameArr.map((element) => {
+      <Grid container sx={{}}>
+        {gameArr.map((element, index) => {
+          let borderStyle =
+            ([3, 4, 5].includes(index) ? "solid " : "none ") +
+            ([1, 4, 7].includes(index) ? "solid" : "none");
           return (
             <Grid
               item
               xs={4}
               sx={{
-                border: "1px solid black",
-                textAlign: "center",
+                justifyContent: "center",
                 width: { xs: "100px", sm: "250px" },
                 height: { xs: "100px", sm: "250px" },
+                display: "flex",
+                alignItems: "center",
+                borderStyle,
+                borderWidth: { xs: "1px", sm: "2px" },
+                borderColor: "black",
+                fontSize: { xs: "30px", sm: "80px" },
               }}
               onClick={handleButtonClick}
             >
