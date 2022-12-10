136e755b18bbebe04d69f1a7a2739104f1508089
Ishan Lalwani <ishn44@gmail.com>
2022-12-10 14:00:57 -0800

    na

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,7 @@
 - Win immediately
 - Block the opponent from an immediate win
 - If there are two intersecting lines where the opponent doesn't control any spots and the bot controls one non-shared spot on each line, take the spot shared by the two lines.
+- If there are two intersecting lines where the bot doesn't control any spots and the opponent controls one non-shared spot on each line, take the spot shared by the two lines.
 - If there are two intersecting lines where the opponent doesn't control any spots and the bot controls only one non-shared spot on one line, take a non-shared spot on the other line.
 - If there are two intersecting lines where the opponent doesn't control any spots, take one one non-shared spot on either of those two lines
 - Take middle spot
@@ -41,7 +42,13 @@ export function botPlay(game, userSymbol) {
   let index = findWinningIndex(game, botSymbol(userSymbol));
   if (index === -1) index = findWinningIndex(game, userSymbol);
   if (index === -1)
-    index = doubleWinStrategy(game, botSymbol(userSymbol), userSymbol);
+    index = doubleWinStrategy(game, botSymbol(userSymbol), userSymbol, 1);
+  if (index === -1)
+    index = doubleWinStrategy(game, userSymbol, botSymbol(userSymbol), 1);
+  if (index === -1)
+    index = doubleWinStrategy(game, botSymbol(userSymbol), userSymbol, 2);
+  if (index === -1)
+    index = doubleWinStrategy(game, botSymbol(userSymbol), userSymbol, 3);
   if (index === -1 && !game[4]) index = 4;
   if (index === -1) index = game.findIndex((e) => !e);
   game[index] = botSymbol(userSymbol);
@@ -94,45 +101,61 @@ function unsharedSpots(line1, line2) {
 
 //console.log(sharedSpot(lines[0], lines[2]));
 
-function doubleWinStrategy(game, botSymbol, userSymbol) {
-  // - If there are two intersecting lines where the opponent doesn't control any spots and the bot controls one non-shared spot on each line, take the spot shared by the two lines.
+function doubleWinStrategy(game, player, opponent, step) {
   for (let line of lines) {
     for (let line2 of intersectingLines(line)) {
-      if (lineScore(game, line, userSymbol)) continue;
-      if (lineScore(game, line2, userSymbol)) continue;
-      if (
-        lineScore(game, unsharedSpots(line2, line), botSymbol) &&
-        lineScore(game, unsharedSpots(line, line2), botSymbol)
-      ) {
-        return sharedSpot(line, line2);
+      if (lineScore(game, line, opponent)) continue;
+      if (lineScore(game, line2, opponent)) continue;
+      switch (step) {
+        case 1:
+          if (
+            lineScore(game, unsharedSpots(line2, line), player) &&
+            lineScore(game, unsharedSpots(line, line2), player)
+          ) {
+            // console.log(game, player, opponent, step, sharedSpot(line, line2));
+            return sharedSpot(line, line2);
+          }
+          break;
+        case 2:
+          if (lineScore(game, unsharedSpots(line, line2), player)) {
+            // console.log(
+            //   game,
+            //   player,
+            //   opponent,
+            //   step,
+            //   unsharedSpots(line2, line)[0]
+            // );
+            return unsharedSpots(line2, line)[0];
+          }
+          if (lineScore(game, unsharedSpots(line2, line), player)) {
+            // console.log(
+            //   game,
+            //   player,
+            //   opponent,
+            //   step,
+            //   unsharedSpots(line, line2)[0]
+            // );
+            return unsharedSpots(line, line2)[0];
+          }
+          break;
+        case 3:
+          // console.log(
+          //   game,
+          //   player,
+          //   opponent,
+          //   step,
+          //   line,
+          //   line2,
+          //   unsharedSpots(line, line2)[0]
+          // );
+          return unsharedSpots(line, line2).filter((elem) => elem !== 4)[0];
       }
     }
   }
-
-  // - If there are two intersecting lines where the opponent doesn't control any spots and the bot controls only one non-shared spot on one line, take a non-shared spot on the other line.
-  for (let line of lines) {
-    for (let line2 of intersectingLines(line)) {
-      if (lineScore(game, line, userSymbol)) continue;
-      if (lineScore(game, line2, userSymbol)) continue;
-      if (lineScore(game, unsharedSpots(line, line2), botSymbol)) {
-        return unsharedSpots(line2, line)[0];
-      }
-      if (lineScore(game, unsharedSpots(line2, line), botSymbol)) {
-        return unsharedSpots(line, line2)[0];
-      }
-    }
-  }
-
-  // - If there are two intersecting lines where the opponent doesn't control any spots, take one one non-shared spot on either of those two lines
-  for (let line of lines) {
-    for (let line2 of intersectingLines(line)) {
-      if (lineScore(game, line, userSymbol)) continue;
-      if (lineScore(game, line2, userSymbol)) continue;
-      return unsharedSpots(line, line2)[0];
-    }
-  }
+  console.log(game, player, opponent, step, -1);
   return -1;
 }
+
 // x| |
 //  |x|
 //  | |
